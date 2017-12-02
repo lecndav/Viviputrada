a3d2c43c972728e5f209cad459b820af88d50319
Michael <michael.dunkel@gmx.at>
2017-12-02 15:09:33 +0100

    mapbox adjustments geojson source issue with mapbox line

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -6,25 +6,53 @@
 import mapboxgl from 'mapbox-gl'
 import turf from '@turf/turf'
 
-const lngLatOfVienna = [16.363449, 48.210033];
-const mapboxTheme = 'mapbox://styles/mapbox/dark-v9';
-const mapboxHtmlContainerID = 'map';
-const mapboxZoomLvl = 11;
+const lngLatOfVienna = [16.363449, 48.210033]; // The inital geographical centerpoint of the map. Note: Mapbox GL uses longitude, latitude coordinate order (as opposed to latitude, longitude) to match GeoJSON.
+const mapboxStyle = 'mapbox://styles/mapbox/dark-v9'; // The map's Mapbox style. This must be an a JSON object conforming to the schema described in the Mapbox Style Specification , or a URL to such JSON.
+const mapboxHtmlContainerID = 'map'; // The HTML element in which Mapbox GL JS will render the map, or the element's string id . The specified element must have no children.
+const mapboxZoomLvl = 11; // The initial zoom level of the map. If  zoom is not specified in the constructor options, Mapbox GL JS will look for it in the map's style object. If it is not specified in the style, either, it will default to  0 .
+const mapboxDragPan = false; // If true, the 'drag to pan' interaction is enabled
+const mapboxDragRotate = true; // If true, the 'drag to rotate' interaction is enabled
+const mapboxBearingSnap = 7; // The threshold, measured in degrees, that determines when the map's bearing (rotation) will snap to north.
 
-/** Mapbox Setup
- * Attention: order = longitude, latitude.
+/** Mapbox Setup   
+ *
  */
 mapboxgl.accessToken = 'pk.eyJ1IjoibWR1bmtlbCIsImEiOiJjamFiM3Yxem8wbmswMzNxdHhoa2w1aWVpIn0.67dORj80QYe7k9CoQg-Fmw';
 var map = new mapboxgl.Map({
-    container: mapboxHtmlContainerID, // ID of container object
-    style: mapboxTheme, // style dark or light
-    center: lngLatOfVienna, // starting position
-    zoom: mapboxZoomLvl // starting zoom, je höher die zahl, desto mehr ist hineingezoomed
+    container: mapboxHtmlContainerID,
+    style: mapboxStyle,
+    center: lngLatOfVienna,
+    zoom: mapboxZoomLvl,
+    pitchWithRotate: mapboxDragPan,
+    dragRotate: mapboxDragRotate,
+    bearingSnap: mapboxBearingSnap
 });
 
 /**
  * draw map
  */
+map.on('load', function(){
+    map.addSource('subways', {
+        'type': 'geojson',
+        'data': './map.geojson'
+        }
+    )
+    map.addLayer({
+        'id': 'subwayLines',
+        'type': 'LineString',
+        'source': 'subways',
+        'layout': {
+            'line-join': 'round',
+            'line-cap': 'round'
+        },
+        'paint': {
+            'line-color': '#888',
+            'line-width': 8
+        }
+    })
+})
+
+/*
 map.on('load', function () {
     map.addLayer({
         'id': 'points',
@@ -65,3 +93,4 @@ map.on('load', function () {
         }
     });
 });
+*/
