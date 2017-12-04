4f3aee1e91210ee70819d9650e5a882961a633fb
MichaelDunkel <michael.dunkel@gmx.at>
2017-12-04 01:49:23 +0100

    included gtfs2geojson converter created new module getData

---
+++ src/assets/js/getData.js
@@ -0,0 +1,8 @@
+/** @file This is the file that gets data gtfs and realtime data over XHR
+ * @version 0.2
+ * @author Michael Dunkel <michael.dunkel@technikum-wien.at>
+ */
+
+import StopsURL from 'index';
+import wlShapesURL from 'index';
+
